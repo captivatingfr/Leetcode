e426a7251a45fd331015f57d0c51302b6dd0dc3c
Rani Thatkar <ranithatkar9@gmail.com>
2026-06-29 23:39:21 +0530

    Time: 0 ms (100%), Space: 43.7 MB (68.99%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+       int l = 0;
+       int r = matrix.length - 1 ;
+       while(l < r){
+        for(int i = 0; i < r - l; i++){
+            int top = l;
+            int bottom = r;
+            //save the top left
+            int topLeft = matrix[top][l + i];
+            //move bottom left to top left
+            matrix[top][l + i] = matrix[bottom - i][l];
+            //move bottom right to bottom left
+            matrix[bottom - i][l] = matrix[bottom][r - i];
+            //move top right to bottom right 
+            matrix[bottom][r - i] = matrix[top + i][r];
+            //move top leftt to top right
+            matrix[top + i][r] = topLeft;
+            
+        }
+        r--;
+        l++;
+       } 
+    }
+}
