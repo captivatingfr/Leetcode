63003a1fec62d2f74b0d2c3f588ec5109a389a4e
Rani Thatkar <ranithatkar9@gmail.com>
2026-07-31 20:05:55 +0530

    Time: 0 ms (100%), Space: 43.1 MB (66.43%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,10 +1,21 @@
 class Solution {
     public int minimumPushes(String word) {
-    int len=word.length();
-        int ans=0;
-        for(int i=0;i<len;i++){
-            ans += (i/8)+1;
+         int[] freq = new int[26];
+        int count = 0;
+        for(int i = 0; i < word.length(); i++){
+            if (i < 8) {
+                count += 1;
+            }
+            else if (i < 16) {
+                count += 2;
+            }
+            else if (i < 24) {
+                count += 3;
+            }
+            else {
+                count += 4;
+            }
         }
-        return ans;
+        return count;
     }
 }
