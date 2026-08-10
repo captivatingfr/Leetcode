7dd6b9d2b3af3985d488f2af72511bee3d7f10ff
Rani Thatkar <ranithatkar9@gmail.com>
2026-08-10 09:05:11 +0530

    Time: 58 ms (94.25%), Space: 43.8 MB (96.28%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -3,18 +3,19 @@ class Solution {
         int n = s.length();
         int sum = 0;
         for(int i = 0; i < s.length(); i++){
-            Map<Character, Integer> freq = new HashMap<>();
+            int[] freq = new int[26];
             for(int j = i; j < s.length(); j++){
-                freq.put(s.charAt(j), freq.getOrDefault(s.charAt(j), 0) + 1);
+                char ch = s.charAt(j);
+                freq[ch - 'a']++;
                 int maxi = Integer.MIN_VALUE;
                 int mini = Integer.MAX_VALUE;
-
-                for (int val : freq.values()) {
-                    mini = Math.min(mini, val);
-                    maxi = Math.max(maxi, val);
-                }
-
-                sum += (maxi - mini);
+                for(int k=0;k<26;k++){
+                        if(freq[k]==0) continue;
+                        
+                        maxi = Math.max(maxi, freq[k]);
+                        mini = Math.min(mini , freq[k]);
+                    }
+                    sum += maxi - mini;
             }
         }
         return sum;
